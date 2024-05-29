5cb336846ffdd0a19bb6426d9407afd20485ac3d
breduarm <bryan.e.armijos@gmail.com>
2024-05-29 10:00:00 -0500

    Initialize ReverseInt problem to be solved

---
+++ app/src/main/java/com/beam/leetcode/solutions/s0007m/ReverseInt.kt
@@ -0,0 +1,8 @@
+package com.beam.leetcode.solutions.s0007m
+
+class ReverseInt {
+
+    fun solve(x: Int): Int {
+        return 0
+    }
+}
---
+++ app/src/test/java/com/beam/leetcode/solutions/s0007m/ReverseIntTest.kt
@@ -0,0 +1,38 @@
+package com.beam.leetcode.solutions.s0007m
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ReverseIntTest {
+    private val reverseInt = ReverseInt()
+
+    @Test
+    fun case_1() {
+        val x = 123
+        val expected = 321
+
+        val result = reverseInt.solve(x)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun case_2() {
+        val x = -123
+        val expected = -321
+
+        val result = reverseInt.solve(x)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun case_3() {
+        val x = 120
+        val expected = 21
+
+        val result = reverseInt.solve(x)
+
+        assertEquals(expected, result)
+    }
+}
