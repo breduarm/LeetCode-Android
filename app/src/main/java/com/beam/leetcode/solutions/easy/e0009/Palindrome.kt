dc69658a083f7452e1b367fa2e3b5fc58925cc83
breduarm <bryan.e.armijos@gmail.com>
2024-06-03 11:10:55 -0500

    Solve Palindrome

--- app/src/main/java/com/beam/leetcode/solutions/easy/e0009/Palindrome.kt
+++ app/src/main/java/com/beam/leetcode/solutions/easy/e0009/Palindrome.kt
@@ -3,6 +3,13 @@ package com.beam.leetcode.solutions.easy.e0009
 class Palindrome {
 
     fun solve(x: Int): Boolean {
-        return false
+        if (x < 0) return false
+        var number = x
+        var result = 0
+        while (number != 0) {
+            result = result * 10 + number % 10
+            number /= 10
+        }
+        return x == result
     }
 }
--- app/src/test/java/com/beam/leetcode/solutions/easy/e0009/PalindromeTest.kt
+++ app/src/test/java/com/beam/leetcode/solutions/easy/e0009/PalindromeTest.kt
@@ -4,13 +4,34 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class PalindromeTest {
+    private val palindrome = Palindrome()
 
     @Test
-    fun true_for_palindrome() {
+    fun check_0_is_palindrome() {
+        val x = 0
+        val expected = true
+
+        val result = palindrome.solve(x)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_negative_int_is_not_palindrome() {
+        val x = -121
+        val expected = false
+
+        val result = palindrome.solve(x)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_121_is_palindrome() {
         val x = 121
         val expected = true
 
-        val result = Palindrome().solve(x)
+        val result = palindrome.solve(x)
 
         assertEquals(expected, result)
     }
