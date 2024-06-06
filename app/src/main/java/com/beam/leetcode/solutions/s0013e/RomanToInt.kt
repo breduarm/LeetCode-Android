099ce97d861c6f6274d56fbabfd4708352a8587b
breduarm <bryan.e.armijos@gmail.com>
2024-06-05 23:26:02 -0500

    Solve RomanToInt problem

--- app/src/main/java/com/beam/leetcode/solutions/s0013e/RomanToInt.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s0013e/RomanToInt.kt
@@ -3,6 +3,28 @@ package com.beam.leetcode.solutions.s0013e
 class RomanToInt {
 
     fun solve(s: String): Int {
-        return 0 // TODO
+        val map = hashMapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000,
+        )
+        var result = 0
+        var prevValue = 0
+
+        for (i in s.length - 1 downTo 0) {
+            val currentValue = map[s[i]] ?: return -1 // return -1 for invalid input
+            if (currentValue < prevValue) {
+                result -= currentValue
+            } else {
+                result += currentValue
+            }
+            prevValue = currentValue
+        }
+
+        return result
     }
 }
--- app/src/test/java/com/beam/leetcode/solutions/s0013e/RomanToIntTest.kt
+++ app/src/test/java/com/beam/leetcode/solutions/s0013e/RomanToIntTest.kt
@@ -17,9 +17,59 @@ class RomanToIntTest {
     }
 
     @Test
-    fun check_roman_number_LVIII_is_58() {
-        val s = "LVIII"
-        val expected = 58
+    fun check_roman_number_II_is_2() {
+        val s = "II"
+        val expected = 2
+
+        val result = romanToInt.solve(s)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_roman_number_XII_is_12() {
+        val s = "XII"
+        val expected = 12
+
+        val result = romanToInt.solve(s)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_roman_number_XXVII_is_27() {
+        val s = "XXVII"
+        val expected = 27
+
+        val result = romanToInt.solve(s)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_roman_number_IV_is_4() {
+        val s = "IV"
+        val expected = 4
+
+        val result = romanToInt.solve(s)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_roman_number_IX_is_9() {
+        val s = "IX"
+        val expected = 9
+
+        val result = romanToInt.solve(s)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_roman_number_MMXXIV_is_2024() {
+        val s = "MMXXIV"
+        val expected = 2024
 
         val result = romanToInt.solve(s)
 
@@ -30,6 +80,16 @@ class RomanToIntTest {
     fun check_roman_number_MCMXCIV_is_1994() {
         val s = "MCMXCIV"
         val expected = 1994
+
+        val result = romanToInt.solve(s)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun check_roman_number_D_is_500() {
+        val s = "D"
+        val expected = 500
 
         val result = romanToInt.solve(s)
 
