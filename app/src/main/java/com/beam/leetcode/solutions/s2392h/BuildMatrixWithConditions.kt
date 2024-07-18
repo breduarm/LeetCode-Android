5565dd9508dd536e6d3335961fa2ab0fbbb2e038
breduarm <bryan.e.armijos@gmail.com>
2024-07-18 11:00:00 -0500

    feat(s2392): Solve BuildMatrixWithCondition problem

--- app/src/main/java/com/beam/leetcode/solutions/s2392h/BuildMatrixWithConditions.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s2392h/BuildMatrixWithConditions.kt
@@ -7,7 +7,49 @@ class BuildMatrixWithConditions {
         rowConditions: Array<IntArray>,
         colConditions: Array<IntArray>,
     ): Array<IntArray> {
+        val rowOrder = topologicalSort(rowConditions, k)
+        val colOrder = topologicalSort(colConditions, k)
 
-        return emptyArray()
+        if (rowOrder.isEmpty() || colOrder.isEmpty()) return emptyArray()
+
+        val result = Array(k) { IntArray(k) }
+        val colMap = colOrder.mapIndexed { index, num -> num to index }.toMap()
+
+        for ((row, num) in rowOrder.withIndex()) {
+            result[row][colMap[num]!!] = num
+        }
+
+        return result
+    }
+
+    private fun topologicalSort(edges: Array<IntArray>, n: Int): List<Int> {
+        val graph = mutableMapOf<Int, MutableList<Int>>()
+        val inDegree = IntArray(n + 1)
+        val order = mutableListOf<Int>()
+
+        for ((before, after) in edges) {
+            graph[before] = graph.getOrDefault(before, mutableListOf())
+            graph[before]!!.add(after)
+            inDegree[after] += 1
+        }
+
+        val toVisit = ArrayDeque((1..n).filter { inDegree[it] == 0 })
+
+        while (toVisit.isNotEmpty()) {
+            val node = toVisit.removeFirst()
+            order.add(node)
+            for (nextNode in graph.getOrDefault(node, emptyList())) {
+                inDegree[nextNode] -= 1
+                if (inDegree[nextNode] == 0) {
+                    toVisit.add(nextNode)
+                }
+            }
+        }
+
+        if (order.size != n) {
+            return emptyList()
+        }
+
+        return order
     }
 }
--- app/src/test/java/com/beam/leetcode/solutions/s2392/BuildMatrixWithConditionsTest.kt
+++ app/src/test/java/com/beam/leetcode/solutions/s2392/BuildMatrixWithConditionsTest.kt
@@ -12,7 +12,7 @@ class BuildMatrixWithConditionsTest {
         val k = 3
         val rowConditions = arrayOf(intArrayOf(1, 2), intArrayOf(3, 2))
         val colConditions = arrayOf(intArrayOf(2, 1), intArrayOf(3, 2))
-        val expected = arrayOf(intArrayOf(3, 0, 0), intArrayOf(0, 0, 1), intArrayOf(0, 2, 0))
+        val expected = arrayOf(intArrayOf(0, 0, 1), intArrayOf(3, 0, 0), intArrayOf(0, 2, 0))
 
         val result = buildMatrixWithConditions.solve(k, rowConditions, colConditions)
 
@@ -22,7 +22,8 @@ class BuildMatrixWithConditionsTest {
     @Test
     fun case_2() {
         val k = 3
-        val rowConditions = arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 1), intArrayOf(2, 3))
+        val rowConditions =
+            arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 1), intArrayOf(2, 3))
         val colConditions = arrayOf(intArrayOf(2, 1))
         val expected = emptyArray<Array<IntArray>>()
 
