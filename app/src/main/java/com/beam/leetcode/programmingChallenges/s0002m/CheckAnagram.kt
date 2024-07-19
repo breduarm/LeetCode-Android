d359afacb1792e58047e02ddeb23829954780df7
breduarm <bryan.e.armijos@gmail.com>
2024-07-19 13:00:00 -0500

    feat(programmingChallenges): Solve CheckAnagram problem

--- app/src/main/java/com/beam/leetcode/programmingChallenges/s0002m/CheckAnagram.kt
+++ app/src/main/java/com/beam/leetcode/programmingChallenges/s0002m/CheckAnagram.kt
@@ -3,6 +3,14 @@ package com.beam.leetcode.programmingChallenges.s0002m
 class CheckAnagram {
 
     fun solve(firstWord: String, secondWord: String): Boolean {
-        return false
+        if (firstWord == secondWord || firstWord.length != secondWord.length) return false
+
+        var result = secondWord
+
+        firstWord.forEach { c ->
+            result = result.replaceFirst(c.toString(), "")
+        }
+
+        return result.isEmpty()
     }
 }
