476a30e75ac3851b27fdf693b63dc5d86a8b058e
breduarm <bryan.e.armijos@gmail.com>
2024-07-22 14:00:00 -0500

    feat(minimumDeletions): Solve  MinimumDeletions

--- app/src/main/java/com/beam/leetcode/solutions/s1653m/MinimumDeletions.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s1653m/MinimumDeletions.kt
@@ -1,8 +1,21 @@
 package com.beam.leetcode.solutions.s1653m
 
+import kotlin.math.min
+
 class MinimumDeletions {
 
     fun solve(s: String): Int {
-        return 0
+        var bCount = 0
+        var minDeletions = 0
+
+        for (char in s) {
+            if (char == 'a') {
+                minDeletions = min(minDeletions + 1, bCount)
+            } else {
+                bCount++
+            }
+        }
+
+        return minDeletions
     }
 }
