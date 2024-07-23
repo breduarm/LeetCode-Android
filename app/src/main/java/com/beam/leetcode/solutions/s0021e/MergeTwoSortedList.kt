e39eec4f5128929e558955a01522c51646ec342e
breduarm <bryan.e.armijos@gmail.com>
2024-07-23 13:00:00 -0500

    feat(mergeTwoSortedList): Solve MergeTwoSortedList

--- app/src/main/java/com/beam/leetcode/solutions/s0021e/MergeTwoSortedList.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s0021e/MergeTwoSortedList.kt
@@ -5,6 +5,27 @@ import com.beam.leetcode.common.ListNode
 class MergeTwoSortedList {
 
     fun solve(list1: ListNode?, list2: ListNode?): ListNode? {
-        return null
+        if (list1 == null && list2 == null) return null
+
+        var l1 = list1
+        var l2 = list2
+        val head = ListNode(-1)
+        var result: ListNode? = head
+
+        while (l1?.`val` != null || l2?.`val` != null) {
+            val l1Value = l1?.`val` ?: Int.MAX_VALUE
+            val l2Value = l2?.`val` ?: Int.MAX_VALUE
+            if (l1Value < l2Value) {
+                result?.next = l1
+                l1 = l1?.next
+                result = result?.next
+            } else {
+                result?.next = l2
+                l2 = l2?.next
+                result = result?.next
+            }
+        }
+
+        return head.next
     }
 }
--- app/src/test/java/com/beam/leetcode/solutions/s0021e/MergeTwoSortedListTest.kt
+++ app/src/test/java/com/beam/leetcode/solutions/s0021e/MergeTwoSortedListTest.kt
@@ -36,6 +36,8 @@ class MergeTwoSortedListTest {
         var result = mergeTwoSortedList.solve(l1Head, l2Head)
 
         while (expected?.next != null || result?.next != null) {
+            println("expected val = ${expected?.`val`}")
+            println("result val = ${result?.`val`}")
             assertEquals(expected?.`val`, result?.`val`)
             expected = expected?.next
             result = result?.next
@@ -48,11 +50,11 @@ class MergeTwoSortedListTest {
     fun case_2() {
         val l1Head: ListNode? = null
         val l2Head: ListNode? = null
-        val expected: ListNode? = null
+        val expected = null
 
         val result = mergeTwoSortedList.solve(l1Head, l2Head)
 
-        assertEquals(expected?.`val`, result?.`val`)
+        assertEquals(expected, result?.`val`)
     }
 
     @Test
@@ -64,5 +66,74 @@ class MergeTwoSortedListTest {
         val result = mergeTwoSortedList.solve(l1Head, l2Head)
 
         assertEquals(expected.`val`, result?.`val`)
+    }
+
+    @Test
+    fun case_4() {
+        val l1Node = ListNode(1)
+        l1Node.next = ListNode(2)
+
+        var l2Node: ListNode? = ListNode(2)
+        val l2Head = l2Node
+        l2Node?.next = ListNode(3)
+        l2Node = l2Node?.next
+        l2Node?.next = ListNode(4)
+
+        var expectedNode: ListNode? = ListNode(1)
+        var expected = expectedNode
+        expectedNode?.next = ListNode(2)
+        expectedNode = expectedNode?.next
+        expectedNode?.next = ListNode(2)
+        expectedNode = expectedNode?.next
+        expectedNode?.next = ListNode(3)
+        expectedNode = expectedNode?.next
+        expectedNode?.next = ListNode(4)
+
+        var result = mergeTwoSortedList.solve(l1Node, l2Head)
+
+        while (expected?.next != null || result?.next != null) {
+            println("expected val = ${expected?.`val`}")
+            println("result val = ${result?.`val`}")
+            assertEquals(expected?.`val`, result?.`val`)
+            expected = expected?.next
+            result = result?.next
+        }
+
+        assertEquals(expected?.`val`, result?.`val`)
+    }
+
+    @Test
+    fun case_5() {
+        val l1Node: ListNode = ListNode(5)
+
+        var l2Node: ListNode? = ListNode(1)
+        val l2Head = l2Node
+        l2Node?.next = ListNode(3)
+        l2Node = l2Node?.next
+        l2Node?.next = ListNode(3)
+        l2Node = l2Node?.next
+        l2Node?.next = ListNode(4)
+
+        var expectedNode: ListNode? = ListNode(1)
+        var expected = expectedNode
+        expectedNode?.next = ListNode(3)
+        expectedNode = expectedNode?.next
+        expectedNode?.next = ListNode(3)
+        expectedNode = expectedNode?.next
+        expectedNode?.next = ListNode(4)
+        expectedNode = expectedNode?.next
+        expectedNode?.next = ListNode(5)
+
+        var result = mergeTwoSortedList.solve(l1Node, l2Head)
+
+        while (expected?.next != null || result?.next != null) {
+            println("expected val = ${expected?.`val`}")
+            println("result val = ${result?.`val`}")
+            assertEquals(expected?.`val`, result?.`val`)
+            expected = expected?.next
+            result = result?.next
+        }
+
+        assertEquals(expected?.`val`, result?.`val`)
     }
 }
