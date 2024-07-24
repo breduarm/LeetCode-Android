5b569bb7fd1abc94db37d227e7eb5c081f3dde5e
breduarm <bryan.e.armijos@gmail.com>
2024-07-24 14:00:00 -0500

    perf(prime): Reduce algorithm complexity from O(n^2) to O(n^(3/2))

--- app/src/main/java/com/beam/leetcode/programmingChallenges/s0004m/Prime.kt
+++ app/src/main/java/com/beam/leetcode/programmingChallenges/s0004m/Prime.kt
@@ -19,11 +19,16 @@ class Prime {
     }
 
     private fun isPrime(n: Int): Boolean {
-        for (i in 2..< n) {
-            if (n % i == 0) {
-                return false
-            }
+        if (n <= 1) return false
+        if (n <= 3) return true
+        if (n % 2 == 0 || n % 3 == 0) return false
+
+        var i = 5
+        while (i * i <= n) {
+            if (n % i == 0 || n % (i + 2) == 0) return false
+            i += 6
         }
+
         return true
     }
 }
