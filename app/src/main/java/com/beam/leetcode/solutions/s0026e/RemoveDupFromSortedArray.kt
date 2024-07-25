f3f1a4169284f157b64c138a2d313eaf3e18e1cd
breduarm <bryan.e.armijos@gmail.com>
2024-07-25 12:00:00 -0500

    feat: Solve RemoveDupFromSortedArray problem

--- app/src/main/java/com/beam/leetcode/solutions/s0026e/RemoveDupFromSortedArray.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s0026e/RemoveDupFromSortedArray.kt
@@ -3,6 +3,22 @@ package com.beam.leetcode.solutions.s0026e
 class RemoveDupFromSortedArray {
 
     fun solve(nums: IntArray): Int {
-        return 0
+        if (nums.isEmpty()) return 0
+        var nextIndexFree = 0
+        var nonDuplicateCount = 1
+
+        for (i in 1..<nums.size) {
+            if (nums[i - 1] == nums[i] || nums[i] < nums[i - 1]) {
+                if (nextIndexFree == 0) nextIndexFree = i
+            } else {
+                if (nextIndexFree > 0) {
+                    nums[nextIndexFree] = nums[i]
+                    nextIndexFree++
+                }
+                nonDuplicateCount++
+            }
+        }
+
+        return nonDuplicateCount
     }
 }
--- app/src/test/java/com/beam/leetcode/solutions/s0026e/RemoveDupFromSortedArrayTest.kt
+++ app/src/test/java/com/beam/leetcode/solutions/s0026e/RemoveDupFromSortedArrayTest.kt
@@ -9,20 +9,90 @@ class RemoveDupFromSortedArrayTest {
     @Test
     fun case_1() {
         val nums = intArrayOf(1, 1, 2)
-        val expected = 2
+        val expectedK = 2
+        val expectedNums = intArrayOf(1, 2)
 
         val result = removeDupFromSortedArray.solve(nums)
 
-        assertEquals(expected, result)
+        assertEquals(expectedK, result)
+
+        for (i in 0..< result) {
+            assertEquals(nums[i], expectedNums[i])
+        }
     }
 
     @Test
     fun case_2() {
         val nums = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 4)
-        val expected = 5
+        val expectedK = 5
+        val expectedNums = intArrayOf(0, 1, 2, 3, 4)
 
         val result = removeDupFromSortedArray.solve(nums)
 
-        assertEquals(expected, result)
+        assertEquals(expectedK, result)
+
+        for (i in 0..< result) {
+            assertEquals(nums[i], expectedNums[i])
+        }
+    }
+
+    @Test
+    fun case_3() {
+        val nums = intArrayOf()
+        val expectedK = 0
+        val expectedNums = intArrayOf()
+
+        val result = removeDupFromSortedArray.solve(nums)
+
+        assertEquals(expectedK, result)
+
+        for (i in 0..< result) {
+            assertEquals(nums[i], expectedNums[i])
+        }
+    }
+
+    @Test
+    fun case_4() {
+        val nums = intArrayOf(1)
+        val expectedK = 1
+        val expectedNums = intArrayOf(1)
+
+        val result = removeDupFromSortedArray.solve(nums)
+
+        assertEquals(expectedK, result)
+
+        for (i in 0..< result) {
+            assertEquals(nums[i], expectedNums[i])
+        }
+    }
+
+    @Test
+    fun case_5() {
+        val nums = intArrayOf(0, 1, 2, 3, 4, 4, 4, 4, 4)
+        val expectedK = 5
+        val expectedNums = intArrayOf(0, 1, 2, 3, 4)
+
+        val result = removeDupFromSortedArray.solve(nums)
+
+        assertEquals(expectedK, result)
+
+        for (i in 0..< result) {
+            assertEquals(nums[i], expectedNums[i])
+        }
+    }
+
+    @Test
+    fun case_6() {
+        val nums = intArrayOf(0, 1, 1, 1, 1, 1, 1, 1, 4)
+        val expectedK = 3
+        val expectedNums = intArrayOf(0, 1, 4)
+
+        val result = removeDupFromSortedArray.solve(nums)
+
+        assertEquals(expectedK, result)
+
+        for (i in 0..< result) {
+            assertEquals(nums[i], expectedNums[i])
+        }
     }
 }
