bf83843aefca15cbf62a4b8dc12e63aad7908814
breduarm <bryan.e.armijos@gmail.com>
2024-07-25 12:00:00 -0500

    refactor: Use the same variable as a counter and index in RemoveDupFromSortedArray

--- app/src/main/java/com/beam/leetcode/solutions/s0026e/RemoveDupFromSortedArray.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s0026e/RemoveDupFromSortedArray.kt
@@ -4,17 +4,12 @@ class RemoveDupFromSortedArray {
 
     fun solve(nums: IntArray): Int {
         if (nums.isEmpty()) return 0
-        var nextIndexFree = 0
+
         var nonDuplicateCount = 1
 
-        for (i in 1..<nums.size) {
-            if (nums[i - 1] == nums[i] || nums[i] < nums[i - 1]) {
-                if (nextIndexFree == 0) nextIndexFree = i
-            } else {
-                if (nextIndexFree > 0) {
-                    nums[nextIndexFree] = nums[i]
-                    nextIndexFree++
-                }
+        for (i in 1..< nums.size) {
+            if (nums[i - 1] != nums[i]) {
+                nums[nonDuplicateCount] = nums[i]
                 nonDuplicateCount++
             }
         }
