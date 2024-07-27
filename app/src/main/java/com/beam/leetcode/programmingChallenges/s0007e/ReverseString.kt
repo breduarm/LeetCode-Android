0fc9b1fe87793115cc5ff27062de53c2e4a6917c
breduarm <bryan.e.armijos@gmail.com>
2024-07-27 14:00:00 -0500

    feat(reverseString): Solve ReverseString

--- app/src/main/java/com/beam/leetcode/programmingChallenges/s0007e/ReverseString.kt
+++ app/src/main/java/com/beam/leetcode/programmingChallenges/s0007e/ReverseString.kt
@@ -3,6 +3,18 @@ package com.beam.leetcode.programmingChallenges.s0007e
 class ReverseString {
 
     fun solve(s: String): String {
-        return ""
+        val charArray: CharArray = s.toCharArray()
+        var left = 0
+        var right = charArray.size - 1
+
+        while (left < right) {
+            val temp = charArray[left]
+            charArray[left] = charArray[right]
+            charArray[right] = temp
+            left++
+            right--
+        }
+
+        return String(charArray)
     }
 }
