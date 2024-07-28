739fa76858c27eafa88f58cebf2aad2d54ac0217
breduarm <bryan.e.armijos@gmail.com>
2024-07-28 14:00:00 -0500

    feat(searchInsertPosition): Solve SearchInsertPosition problem

--- app/src/main/java/com/beam/leetcode/solutions/s0035e/SearchInsertPosition.kt
+++ app/src/main/java/com/beam/leetcode/solutions/s0035e/SearchInsertPosition.kt
@@ -3,6 +3,20 @@ package com.beam.leetcode.solutions.s0035e
 class SearchInsertPosition {
 
     fun solve(nums: IntArray, target: Int): Int {
-        return 0
+        var left = 0
+        var right = nums.size - 1
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+
+            when {
+                nums[mid] == target -> return mid // If the target is found, return its index
+                nums[mid] < target -> left = mid + 1 // the target is in the right half
+                else -> right = mid - 1 // the target is in the left half
+            }
+        }
+
+        // If the target is not found, return the index where it should be inserted
+        return left
     }
 }
