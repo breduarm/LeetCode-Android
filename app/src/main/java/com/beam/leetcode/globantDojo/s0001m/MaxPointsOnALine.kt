7f42bf436356b066281d67f58db12a9e3ba49cd6
breduarm <bryan.e.armijos@gmail.com>
2024-10-01 15:36:38 -0500

    feat(globantDojo): Solve s0001m

--- app/src/main/java/com/beam/leetcode/globantDojo/s0001m/MaxPointsOnALine.kt
+++ app/src/main/java/com/beam/leetcode/globantDojo/s0001m/MaxPointsOnALine.kt
@@ -1,8 +1,38 @@
 package com.beam.leetcode.globantDojo.s0001m
 
+import kotlin.math.max
+
 class MaxPointsOnALine {
 
     fun solve(points: Array<IntArray>): Int {
-        return 0
+        if (points.size <= 2) return points.size
+
+        var maxPoints = 0
+
+        // Iterate through each point
+        for (i in points.indices) {
+            val slopes = mutableMapOf<Pair<Int, Int>, Int>()
+            var currentMax = 0
+
+            // Iterate through the remaining points in order to find the all possible slopes
+            for (j in i + 1 until points.size) {
+                val xDiff = points[j][0] - points[i][0]
+                val yDiff = points[j][1] - points[i][1]
+
+                val gcd = gcd(xDiff, yDiff)
+                val slope = Pair(xDiff / gcd, yDiff / gcd)
+
+                slopes[slope] = slopes.getOrDefault(slope, 0) + 1
+                currentMax = max(currentMax, slopes[slope] ?: 0)
+            }
+
+            maxPoints = max(maxPoints, currentMax)
+        }
+
+        return maxPoints + 1 // Add 1 for the current point
     }
+
+    // Calculate the greatest common divisor (GCD)
+    private fun gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd( a= b, b = a % b)
 }
--- app/src/test/java/com/beam/leetcode/globantDojo/s0001m/MaxPointsOnALineTest.kt
+++ app/src/test/java/com/beam/leetcode/globantDojo/s0001m/MaxPointsOnALineTest.kt
@@ -36,4 +36,36 @@ class MaxPointsOnALineTest {
 
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun case_3() {
+        val points = arrayOf(
+            intArrayOf(5, 7),
+            intArrayOf(1, 1),
+            intArrayOf(2, 1),
+            intArrayOf(2, 2),
+            intArrayOf(6, 6),
+            intArrayOf(3, 3)
+        )
+        val expected = 4
+
+        val actual = maxPointsOnALine.solve(points)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun case_4() {
+        val points = arrayOf(
+            intArrayOf(5, 1),
+            intArrayOf(5, 3),
+            intArrayOf(5, 9),
+            intArrayOf(6, 1),
+        )
+        val expected = 3
+
+        val actual = maxPointsOnALine.solve(points)
+
+        assertEquals(expected, actual)
+    }
 }
